c83ccde27bc5736a326f0ca995f5dac7c88f6c2f
masjinno <masjinno@gmail.com>
2022-01-09 01:59:38 +0900

    入力欄実装

--- src/App.js
+++ src/App.js
@@ -23,11 +23,13 @@ import './App.css';
 //   );
 // }
 
+const GET_PDF_PROPERTIES_URL = "https://gx8vyib51l.execute-api.us-west-2.amazonaws.com/dev";
+
 class CsvInput extends React.Component {
   render() {
     return (
       <p>
-        <label for="csv_file">
+        <label>
           CSVファイル
           <input type="file" id="csv_file" accept="text/csv"/>
         </label>
@@ -37,37 +39,49 @@ class CsvInput extends React.Component {
 }
 
 class PageSetting extends React.Component {
+  constructor(props) {
+    super(props);
+    this.pageSizes = props.pageSizes;
+    this.orientations = props.orientations;
+  }
+
   render() {
+    const pageSizeTags = this.pageSizes.map(pageSize =>
+      <option>{pageSize}</option>
+    );
+
+    let orientationTags = this.orientations.map(orientation =>
+      <label><input type="radio" name="orientation"/>{orientation}</label>
+    );
+
     return (
       <div>
         <h2>ページ設定</h2>
         <p>
-          <label for="page_size">
+          <label>
             用紙サイズ
             <select>
-              <option>a</option>
-              <option>b</option>
+              {pageSizeTags}
             </select>
           </label>
         </p>
         <p>
           <label>用紙の向き</label>
-          <label><input type="radio" name="orientation" value="a"/>縦向き</label>
-          <label><input type="radio" name="orientation" value="b"/>横向き</label>
+          {orientationTags}
         </p>
         <p>
           <label>余白</label>
-          <label for="margin_top">上
-            <input type="text" id="margin_top"/>
+          <label>上
+            <input type="text"/>
           </label>
-          <label for="margin_left">左
-            <input type="text" id="margin_left"/>
+          <label>左
+            <input type="text"/>
           </label>
-          <label for="margin_right">右
-            <input type="text" id="margin_right"/>
+          <label>右
+            <input type="text"/>
           </label>
-          <label for="margin_bottom">下
-            <input type="text" id="margin_bottom"/>
+          <label>下
+            <input type="text"/>
           </label>
         </p>
       </div>
@@ -76,42 +90,52 @@ class PageSetting extends React.Component {
 }
 
 class CsvHeaderSetting extends React.Component {
+  constructor(props) {
+    super(props)
+    this.fontFamilies = props.fontFamilies;
+  }
+
   render() {
+    const fontFamilyTags = this.fontFamilies.map(fontFamily => 
+      <option>{fontFamily}</option>
+    );
     return (
       <div>
         <h2>CSVヘッダ設定</h2>
         <p>
-          <label for="header_font_size">
+          <label>
             フォントサイズ
-            <input type="text" id="header_font_size"/>
+            <input type="text"/>
           </label>
         </p>
         <p>
-          <label for="header_font_family">
+          <label>
             フォント名
-            <input type="text" id="header_font_family"/>
+            <select>
+              {fontFamilyTags}
+            </select>
           </label>
         </p>
         <p>
-          <label for="header_markup_start">
+          <label>
             ヘッダマークの先頭
-            <input type="text" id="header_markup_start"/>
+            <input type="text"/>
           </label>
         </p>
         <p>
-          <label for="header_markup_end">
+          <label>
             ヘッダマークの末尾
-            <input type="text" id="header_markup_end"/>
+            <input type="text"/>
           </label>
         </p>
         <p>
           <label>
             出力対象項目
             <label>
-              <input type="checkbox" id="header_markup_end"/>aaa
+              <input type="checkbox"/>aaa
             </label>
             <label>
-              <input type="checkbox" id="header_markup_end"/>bbb
+              <input type="checkbox"/>bbb
             </label>
           </label>
         </p>
@@ -121,20 +145,30 @@ class CsvHeaderSetting extends React.Component {
 }
 
 class CsvContentSetting extends React.Component {
+  constructor(props) {
+    super(props)
+    this.fontFamilies = props.fontFamilies;
+  }
+
   render() {
+    const fontFamilyTags = this.fontFamilies.map(fontFamily => 
+      <option>{fontFamily}</option>
+    );
     return (
       <div>
         <h2>CSV内容の設定</h2>
         <p>
-          <label for="content_font_size">
+          <label>
             フォントサイズ
-            <input type="text" id="header_font_size"/>
+            <input type="text"/>
           </label>
         </p>
         <p>
-          <label for="content_font_family">
+          <label>
             フォント名
-            <input type="text" id="header_font_family"/>
+            <select>
+              {fontFamilyTags}
+            </select>
           </label>
         </p>
       </div>
@@ -153,15 +187,59 @@ class Conversion extends React.Component {
 }
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      pdfProperty: null
+    };
+  }
+
+  getProperty() {
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+    var requestOptions = {
+        method: 'GET',
+        headers: myHeaders
+    };
+    fetch(GET_PDF_PROPERTIES_URL, requestOptions)
+      .then(response => response.json())
+      .then(responseJson => {
+        this.setState(
+          {
+            pdfProperty: responseJson
+          }
+        );
+      })
+      .catch(error => console.log('error', error));
+  }
+
+  componentDidMount() {
+    this.getProperty();
+  }
+
   render() {
+    if (!this.state.pdfProperty) {
+      return <div>...Loading</div>;
+    }
     return (
       <div>
         <h1>CSV 2 PDF</h1>
         <CsvInput/>
-        <PageSetting/>
-        <CsvHeaderSetting/>
-        <CsvContentSetting/>
+        <PageSetting
+          pageSizes={this.state.pdfProperty.PageSizes}
+          orientations={this.state.pdfProperty.Orientations}/>
+        <CsvHeaderSetting fontFamilies={this.state.pdfProperty.FontFamilies}/>
+        <CsvContentSetting fontFamilies={this.state.pdfProperty.FontFamilies}/>
         <Conversion/>
+        <div>
+          <h2>デバッグエリア</h2>
+          <p>
+            <label>
+              getPdfProperty
+              <textarea value={JSON.stringify(this.state.pdfProperty)} readOnly/>
+            </label>
+          </p>
+        </div>
       </div>
     );
   }
