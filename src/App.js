4131c87ae4331a501a976733cc95d15b4d041fe2
masjinno <masjinno@gmail.com>
2022-02-14 08:28:22 +0900

    プロジェクト新規作成時に戻す

--- src/App.js
+++ src/App.js
@@ -1,110 +1,23 @@
-// import logo from './logo.svg';
-import React, {useState, useEffect} from 'react';
+import logo from './logo.svg';
 import './App.css';
-import CsvInput from './CsvInput.js';
-import PageSetting from './PageSetting.js';
-import CsvHeaderSetting from './CsvHeaderSetting.js';
-import CsvContentSetting from './CsvContentSetting.js';
-import Conversion from './Conversion.js';
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-const GET_PDF_PROPERTIES_URL = "https://gx8vyib51l.execute-api.us-west-2.amazonaws.com/dev/";
-
-const App = () => {
-  const [pdfProperty, setPdfProperty] = useState(null);
-  const [csvData, setCsvData] = useState(null);
-  const [csvHeaderItems, setCsvHeaderItems] = useState(null);
-  const [pageSize, setPageSize] = useState(null);
-  const [pageOrientation, setPageOrientation] = useState(null);
-  const [pageMarginTop, setPageMarginTop] = useState(null);
-  const [pageMarginLeft, setPageMarginLeft] = useState(null);
-  const [pageMarginRight, setPageMarginRight] = useState(null);
-  const [pageMarginBottom, setPageMarginBottom] = useState(null);
-  const [headerFontSize, setHeaderFontSize] = useState(null);
-  const [headerFontFamily, setHeaderFontFamily] = useState(null);
-  const [headerMarkupStart, setHeaderMarkupStart] = useState(null);
-  const [headerMarkupEnd, setHeaderMarkupEnd] = useState(null);
-  const [targetItems, setTargetItems] = useState(null);
-  const [contentFontSize, setContentFontSize] = useState(null);
-  const [contentFontFamily, setContentFontFamily] = useState(null);
-
-  // 初回のみプロパティ取得を実行
-  useEffect(() => {
-    let myHeaders = new Headers();
-    myHeaders.append("Content-Type", "application/json");
-    let requestOptions = {
-        method: 'GET',
-        headers: myHeaders
-    };
-    console.log("GetPdfProperties calling");
-    fetch(GET_PDF_PROPERTIES_URL, requestOptions)
-      .then(response => response.json())
-      .then(responseJson => setPdfProperty(responseJson))
-      .catch(error => console.log('error', error));
-  }, []);
-
-  if (!pdfProperty) {
-    return <div>...Loading</div>;
-  }
-
-  const pageMargin = [pageMarginTop, pageMarginLeft, pageMarginRight, pageMarginBottom];
+function App() {
   return (
-    <div>
-      <h1>CSV 2 PDF</h1>
-      <CsvInput setCsvData={setCsvData} setCsvHeaderItems={setCsvHeaderItems}/>
-      <PageSetting
-        pageSizes={pdfProperty.PageSizes}
-        setPageSize={setPageSize}
-        orientations={pdfProperty.Orientations}
-        setPageOrientation={setPageOrientation}
-        setPageMarginTop={setPageMarginTop}
-        setPageMarginLeft={setPageMarginLeft}
-        setPageMarginRight={setPageMarginRight}
-        setPageMarginBottom={setPageMarginBottom}/>
-      <CsvHeaderSetting
-        setFontSize={setHeaderFontSize}
-        fontFamilies={pdfProperty.FontFamilies}
-        setFontFamily={setHeaderFontFamily}
-        setMarkupStart={setHeaderMarkupStart}
-        setMarkupEnd={setHeaderMarkupEnd}
-        targetItems={csvHeaderItems}
-        setTargetItems={setTargetItems}/>
-      <CsvContentSetting
-        setFontSize={setContentFontSize}
-        fontFamilies={pdfProperty.FontFamilies}
-        setFontFamily={setContentFontFamily}/>
-      <Conversion
-        csvData={csvData}
-        pageSize={pageSize}
-        pageOrientation={pageOrientation}
-        pageMargin={pageMargin}
-        headerFontSize={headerFontSize}
-        headerFontFamily={headerFontFamily}
-        headerMarkupStart={headerMarkupStart}
-        headerMarkupEnd={headerMarkupEnd}
-        targetItems={targetItems}
-        contentFontSize={contentFontSize}
-        contentFontFamily={contentFontFamily}/>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
